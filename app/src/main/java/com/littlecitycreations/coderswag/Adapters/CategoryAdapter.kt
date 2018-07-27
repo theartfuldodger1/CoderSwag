32bab11534ba3881f8b7991e8b59b9535f9c7171
Cordell Hurst <16294570+theartfuldodger1@users.noreply.github.com>
2018-07-27 18:36:09 -0400

    Added CategoryAdapter.kt

---
+++ app/src/main/java/com/littlecitycreations/coderswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,65 @@
+package com.littlecitycreations.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.littlecitycreations.coderswag.Model.Category
+import com.littlecitycreations.coderswag.R
+
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+
+    val context = context
+    val categories = categories
+
+    override fun getView(position : Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView: View
+        val holder : ViewHolder
+
+        if (convertView == null) {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            println("I exist for the first time!")
+            categoryView.tag = holder
+        }
+        else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+            println("I'm Recycled!")
+        }
+        //heavy computing because inflating view every scroll
+        //using ViewHolder() here recycles the view rather than creating new ones every time
+
+        val category = categories[position]
+
+        val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        holder.categoryImage?.setImageResource(resourceId)
+
+        holder.categoryName?.text = category.title
+
+        return categoryView
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
+    }
+}
--- app/src/main/java/com/littlecitycreations/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/littlecitycreations/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.littlecitycreations.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.Dataset
+import android.widget.ArrayAdapter
+import com.littlecitycreations.coderswag.Adapters.CategoryAdapter
+import com.littlecitycreations.coderswag.Model.Category
 import com.littlecitycreations.coderswag.R
+import com.littlecitycreations.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
