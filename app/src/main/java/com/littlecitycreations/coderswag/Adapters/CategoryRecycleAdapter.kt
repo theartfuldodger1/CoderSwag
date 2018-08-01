9fe3c03d4a7ea82c48c14e2feae3d892ddcba2be
Cordell Hurst <16294570+theartfuldodger1@users.noreply.github.com>
2018-08-01 01:34:28 -0400

    started recycle adapter 2

--- app/src/main/java/com/littlecitycreations/coderswag/Adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/com/littlecitycreations/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -1,4 +1,24 @@
 package com.littlecitycreations.coderswag.Adapters
 
-class CategoryRecycleAdapter {
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.littlecitycreations.coderswag.Model.Category
+
+class CategoryRecycleAdapter(val context : Context, val categories: List<Category>) : RecyclerView.Adapter <CategoryRecycleAdapter.holder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): holder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: holder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class holder(itemView: View?) : RecyclerView.ViewHolder(itemView)
 }
